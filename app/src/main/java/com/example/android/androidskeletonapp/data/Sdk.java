30df8d2ecdfc1ea245dab1073d49782e211f5a71
achalajayatilleke <achalauj@gmail.com>
2019-07-02 15:52:09 +0530

    Solution

--- app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
+++ app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
@@ -8,7 +8,12 @@ public class Sdk {
 
     public static D2Configuration getD2Configuration(Context context) {
         // TODO Add configuration
-
-        return null;
+        D2Configuration configuration = D2Configuration.builder()
+                .appName("achalajayatilleke").appVersion("1.0.0")
+                .context(context)
+                .readTimeoutInSeconds(30).connectTimeoutInSeconds(30).writeTimeoutInSeconds(30)
+                //.networkInterceptors(Collections.singletonList(new StethoInterceptor()))
+                .build();
+                return configuration;
     }
 }
